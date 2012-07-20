3eaca6d26ff3babf8201c8747d46855fa1f75d5e
SriramK89 <ksrirambecse@gmail.com>
2012-07-20 16:19:01 +0530

    Finished user manipulation actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,25 @@
 class SessionsController < ApplicationController
 
   def new
+    if signed_in?
+      flash[:notice] = "Sorry! That was an unauthorized action"
+      redirect_to current_user
+    end
   end
 
   def create
-  	user = User.find_by_email(params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
-      sign_in user
-      redirect_to user
+  	if signed_in?
+      flash[:notice] = "Sorry! That was an unauthorized action"
+      redirect_to current_user
     else
-      flash.now[:error] = "Invalid email/password combination"
-      render 'new'
+      user = User.find_by_email(params[:session][:email])
+      if user && user.authenticate(params[:session][:password])
+        sign_in user
+        redirect_back_or user
+      else
+        flash.now[:error] = "Invalid email/password combination"
+        render 'new'
+      end
     end
   end
 
