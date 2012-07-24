2bdab075dfda092ea35b5731f3ab7d862de689e3
SriramK89 <ksrirambecse@gmail.com>
2012-07-24 16:47:20 +0530

    Finished following users

--- spec2/models/user_spec.rb
+++ spec2/models/user_spec.rb
@@ -1,20 +1,11 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
-#
-
 require 'spec_helper'
 
 describe User do
-  
-  before { @user = User.new(name: "Example User", email: "user@example.com", 
-                   password: "foobar", password_confirmation: "foobar") }
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
 
@@ -24,6 +15,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
@@ -38,34 +30,38 @@ describe User do
   it { should be_valid }
   it { should_not be_admin }
 
+  describe "accessible attributes" do
+    it "should not allow access to admin" do
+      expect do
+        User.new(admin: true)
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end    
+  end
+
   describe "with admin attribute set to 'true'" do
     before { @user.toggle!(:admin) }
 
     it { should be_admin }
   end
-
+  
   describe "when name is not present" do
-  	before { @user.name = "" }
-
-  	it { should_not be_valid }
+    before { @user.name = " " }
+    it { should_not be_valid }
   end
 
   describe "when email is not present" do
-  	before { @user.email = "" }
-
-  	it { should_not be_valid }
+    before { @user.email = " " }
+    it { should_not be_valid }
   end
 
   describe "when name is too long" do
-  	before { @user.name = "a" * 51 }
-
-  	it { should_not be_valid }
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
   end
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         @user.should_not be_valid
@@ -93,29 +89,57 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when password is not present" do
-  	before { @user.password = @user.password_confirmation = "" }
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
-  	it { should_not be_valid }
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
   end
 
-  describe "when password confirmation is nil" do
-  	before { @user.password_confirmation = nil }
-
-  	it { should_not be_valid }
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
   end
 
   describe "when password doesn't match confirmation" do
-  	before { @user.password_confirmation = "mismatch" }
-
-  	it { should_not be_valid }
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
   end
 
-  describe "with a password that is too short" do
-  	before { @user.password = "a" * 5 }
-
-  	it { should_not be_valid }
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
   end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
 
   describe "micropost associations" do
 
@@ -130,7 +154,7 @@ describe User do
     it "should have the right microposts in the right order" do
       @user.microposts.should == [newer_micropost, older_micropost]
     end
-
+  
     it "should destroy associated microposts" do
       microposts = @user.microposts
       @user.destroy
@@ -138,11 +162,12 @@ describe User do
         Micropost.find_by_id(micropost.id).should be_nil
       end
     end
-
+  
     describe "status" do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+
       let(:followed_user) { FactoryGirl.create(:user) }
 
       before do
--- spec2/requests/authentication_pages_spec.rb
+++ spec2/requests/authentication_pages_spec.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe "Authentication" do
 
@@ -7,8 +7,8 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_selector("h1",    text: "Sign in") }
-    it { should have_selector("title", text: "Sign in") }
+    it { should have_selector('h1',    text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
   end
 
   describe "signin" do
@@ -17,10 +17,15 @@ describe "Authentication" do
     describe "with invalid information" do
       before { click_button "Sign in" }
 
-      it { should have_selector("title", text: "Sign in") }
-      it { should have_selector("div.alert.alert-error", text: "Invalid") }
-    end
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
+    
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
@@ -31,7 +36,7 @@ describe "Authentication" do
 
       it { should have_selector('title', text: user.name) }
 
-      it { should have_link('Users', href: users_path) }
+      it { should have_link('Users',    href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
@@ -62,19 +67,20 @@ describe "Authentication" do
           it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
           end
-        end
-      end
 
-      describe "in the Microposts controller" do
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
 
-        describe "submitting to the create action" do
-          before { post microposts_path }
-          specify { response.should redirect_to(signin_path) }
-        end
-
-        describe "submitting to the destroy action" do
-          before { delete micropost_path(FactoryGirl.create(:micropost)) }
-          specify { response.should redirect_to(signin_path) }
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name) 
+            end
+          end
         end
       end
 
@@ -90,7 +96,7 @@ describe "Authentication" do
           specify { response.should redirect_to(signin_path) }
         end
 
-        describe "visiting the user index" do
+        describe "visiting user index" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
@@ -103,6 +109,19 @@ describe "Authentication" do
         describe "visiting the followers page" do
           before { visit followers_user_path(user) }
           it { should have_selector('title', text: 'Sign in') }
+        end
+      end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
         end
       end
 
@@ -126,12 +145,24 @@ describe "Authentication" do
 
       describe "visiting Users#edit page" do
         before { visit edit_user_path(wrong_user) }
-        it { should_not have_selector('title', text: full_title('Edit user')) }
+        it { should have_selector('title', text: full_title('')) }
       end
 
       describe "submitting a PUT request to the Users#update action" do
         before { put user_path(wrong_user) }
         specify { response.should redirect_to(root_path) }
+      end
+    end
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }        
       end
     end
   end
