aaef53ee033f9a3144f15f7648b9ce5fbb800b01
Sriram Kumar <sriram.k2@tcs.com>
2012-07-27 15:52:12 +0530

    Completed subset count for pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: 15)
     @profile_link_display_off = true
   end
 
@@ -61,14 +61,14 @@ class UsersController < ApplicationController
   def following
     @title = "Following"
     @user = User.find(params[:id])
-    @users = @user.followed_users.paginate(page: params[:page])
+    @users = @user.followed_users.paginate(page: params[:page], per_page: 15)
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
     @user = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
+    @users = @user.followers.paginate(page: params[:page], per_page: 15)
     render 'show_follow'
   end
 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,10 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  def pagination_subset_items_display(page_number, fixed_item_count, page_item_count, total_items, item_name)
+  	start_item = (( page_number - 1 ) * fixed_item_count ) + 1
+  	end_item = start_item + ( page_item_count - 1 )
+  	"Showing #{start_item} to #{end_item} of #{total_items} #{item_name}"
+  end
 end
