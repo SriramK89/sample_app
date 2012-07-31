93f5a7a065fbea131f82d52fd78fc49455fdd384
Sriram Kumar <sriram.k2@tcs.com>
2012-07-31 19:00:44 +0530

    Changed .js.erb to .js.slim and edited pagination details

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,16 @@
 class CommentsController < ApplicationController
-  before_filter :correct_user
 
   def create
-  	@micropost = Micropost.find(params[:comment][:micropost_id])
+    @micropost = Micropost.find(params[:comment][:micropost_id])
   	@content = params[:comment][:content]
+    @user = User.find(@micropost.user_id)
+    @comments = @micropost.comments.paginate(page: params[:page], per_page: 10)
   	begin
 	  	current_user.create_comment!(@micropost, @content)
-	rescue
-		flash[:error] = "Content of the comment can't be blank"
-	end
-		redirect_to request.referer
+  	rescue
+  		flash[:error] = "Content of the comment can't be blank"
+  	end
+    redirect_to request.referer
   end
 
-  private
-
-    def correct_user
-      @user = User.find(params[:comment][:user_id])
-      redirect_to(root_path, notice: "Sorry! That was an unauthorized action") unless current_user?(@user)
-    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,6 +9,14 @@ module UsersHelper
   def pagination_subset_items_display(page_number, fixed_item_count, page_item_count, total_items, item_name)
   	start_item = (( page_number - 1 ) * fixed_item_count ) + 1
   	end_item = start_item + ( page_item_count - 1 )
-  	"Showing #{start_item} to #{end_item} of #{total_items} #{item_name}"
+    if start_item == end_item
+      "Showing #{start_item} of " + pluralize(total_items, item_name)
+    else
+      if (start_item + 1) == end_item
+        "Showing #{start_item} and #{end_item} of " + pluralize(total_items, item_name)
+      else
+      	"Showing #{start_item} to #{end_item} of " + pluralize(total_items, item_name)
+      end
+    end
   end
 end
